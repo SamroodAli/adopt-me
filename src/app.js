49c4c2740d419096ac397a1452a5e15d7b598946
samroodAli <devsamrood@gmail.com>
2021-03-14 01:02:54 +0530

    readability:remove the react.createElement comment lines

--- src/app.js
+++ src/app.js
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// const Pet = (props) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h2", {}, props.name),
-//     React.createElement("h3", {}, props.animal),
-//     React.createElement("h3", {}, props.breed),
-//   ]);
-// };
-
 const Pet = (props) => {
   return (
     <div>
@@ -18,28 +10,6 @@ const Pet = (props) => {
     </div>
   );
 };
-
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me"),
-//     React.createElement(Pet, {
-//       name: "Puss",
-//       animal: "Cat",
-//       breed: "Tabby",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Rio",
-//       animal: "Parrot",
-//       breed: "Spix's Macaw",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Bolt",
-//       animal: "Dog",
-//       breed: "German Shepherd",
-//     }),
-//   ]);
-// };
-
 
 const App = ()=>{
   return(
