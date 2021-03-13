b7ae914e4a9fb61b05fd1758aa619ba8119f0f4a
samroodAli <devsamrood@gmail.com>
2021-03-13 16:29:21 +0530

    components:Pet component in app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const Pet = (name) => {
+const Pet = () => {
   return React.createElement("div", {}, [
     React.createElement("h2", {}, "Luna"),
     React.createElement("h3", {}, "Dog"),
@@ -9,9 +9,21 @@ const Pet = (name) => {
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, {
+      name: "Puss",
+      animal: "Cat",
+      breed: "Tabby",
+    }),
+    React.createElement(Pet, {
+      name: "Rio",
+      animal: "Parrot",
+      breed: "Spix's Macaw",
+    }),
+    React.createElement(Pet, {
+      name: "Bolt",
+      animal: "Dog",
+      breed: "German Shepherd",
+    }),
   ]);
 };
 
