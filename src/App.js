a754016acb2153b56e8091de785eeb895c1016eb
samroodAli <devsamrood@gmail.com>
2021-03-14 22:10:12 +0530

    files:updated file names to capitalise first character

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanenese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Beam" animal="Dog" breed="Wheaten Terrier" />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
