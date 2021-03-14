a30f556835333d14e9f7ed2dbe4594a285f8e305
samroodAli <devsamrood@gmail.com>
2021-03-14 22:17:15 +0530

    component:searchParams component to hook search queries in site

--- src/SearchParam.js
+++ src/SearchParam.js
@@ -1,5 +1,15 @@
-const SearchPArams = ()=>{
+const location ="Kerala,India"
+const SearchParams = ()=>{
   return(
-    
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">Location
+        <input type="text" id="location" value={location.toUpperCase()} placeholder="location"/>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
+
+export default SearchParams;
