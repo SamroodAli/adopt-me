693367a1c7142e6ae9d706cfa26f7e1366e9ff64
samroodAli <devsamrood@gmail.com>
2021-03-15 09:49:36 +0530

    state:setLocation and location in SearchParams componenet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParam";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanenese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Beam" animal="Dog" breed="Wheaten Terrier" />
+      <SearchParams />
     </div>
   );
 };
--- src/SearchParam.js
+++ src/SearchParam.js
@@ -1,15 +1,24 @@
-const location ="Kerala,India"
-const SearchParams = ()=>{
-  return(
+import { useState } from "react";
+
+const SearchParams = () => {
+  let [location, setLocation] = useState("Kerala,India");
+  return (
     <div className="search-params">
       <form>
-        <label htmlFor="location">Location
-        <input type="text" id="location" value={location.toUpperCase()} placeholder="location"/>
+        <label htmlFor="location">
+          Location
+          <input
+            type="text"
+            id="location"
+            value={location.toUpperCase()}
+            onChange={(e) => setLocation(e.target.value)}
+            placeholder="location"
+          />
         </label>
         <button>Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default SearchParams;
