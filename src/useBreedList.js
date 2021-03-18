eecbef00ca8b25213f675a066b407bc144faebb2
samroodAli <devsamrood@gmail.com>
2021-03-18 09:50:50 +0530

    custom-hook:using effect in useBreedList with memoization

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -5,4 +5,14 @@ const localCache = {};
 export default function useBreedList(animal) {
   const [breedList, setBreedList] = useState([]);
   const [status, setStatus] = useState("unloaded");
+
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
+  });
 }
