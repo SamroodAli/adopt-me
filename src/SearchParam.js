53da01658c05e81597e14ed8e4a79cd1d71f0cea
samroodAli <devsamrood@gmail.com>
2021-03-18 12:30:51 +0530

    Results:refactored pet results to a component Results

--- src/SearchParam.js
+++ src/SearchParam.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import Pet from "./Pet";
 import useBreedList from "./useBreedList";
+import Results from "./Results";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -90,14 +90,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
