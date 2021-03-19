c0564de370a079c1215cf7a70e61b8d32a921076
samroodAli <devsamrood@gmail.com>
2021-03-19 08:26:32 +0530

    Pet:refactored pet, include images and pet data

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,19 @@
 const Pet = ({ name, animal, breed, images, location, id }) => {
-  let hero = `http://pets-images.dev-apis.com/pets/none.jpg`;
+  let hero = `http://pets-images.dev-apis.com/pets/none.jpg`; //in case no images are fetched -defensive programming
+  if (images.length) {
+    hero = images[0];
+  }
+
   return (
-    <div>
-      <h2>{name}</h2>
-      <h3>{animal}</h3>
-      <h3>{breed}</h3>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div>
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
 };
 
