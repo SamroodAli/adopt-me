6280e480d8e5a02fd1bd4efb070ad43cf9cf4da5
samroodAli <devsamrood@gmail.com>
2021-03-19 08:31:22 +0530

    Pets:give class name info pet info div

--- src/Pet.js
+++ src/Pet.js
@@ -9,7 +9,7 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
-      <div>
+      <div className="info">
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
