44785ac9800a5b03a25b2eede184ff584b4439e8
samroodAli <devsamrood@gmail.com>
2021-03-19 12:17:27 +0530

    switch:introduce switch in router for rendering only path specified components

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParam";
 import Details from "./Details.js";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
       <Router>
-        <Route path="/details/:id">
-          <Details />
-        </Route>
-        <Route path="/">
-          <SearchParams />
-        </Route>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
