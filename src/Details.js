e24d66b11f1bb7901c1e21983c766bf89037811d
samroodAli <devsamrood@gmail.com>
2021-03-20 23:20:45 +0530

    Component:Details component changed to class component

--- src/Details.js
+++ src/Details.js
@@ -1,4 +1,14 @@
-const Details = () => {
-  return <h2>Hi</h2>;
-};
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <h2>Hi</h2>;
+  }
+}
+
 export default Details;
