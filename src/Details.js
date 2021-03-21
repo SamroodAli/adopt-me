77ed8b1097963ffa66c21e5839dea024d6923d94
samroodAli <devsamrood@gmail.com>
2021-03-21 07:43:05 +0530

    component lifecycle method componentDidMount for Details component

--- src/Details.js
+++ src/Details.js
@@ -4,6 +4,23 @@ import { withRouter } from "react-router-dom";
 class Details extends Component {
   constructor() {
     super();
+
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
   }
 
   render() {
