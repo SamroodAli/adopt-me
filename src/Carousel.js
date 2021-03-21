77563cbeddf1404dcd06340dd1375f16e7f14ab8
samroodAli <devsamrood@gmail.com>
2021-03-21 23:04:59 +0530

    class-component:image carousel and default props using static defaultProps
    defaultProps is the way to set defaults for props, props which are state
    coming from parent, and static means it is only available in Class, it
    becomes read only in class instances/components.

---
+++ src/Carousel.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class Carousel extends Component {
+  state = { active: 0 }; //photo at index 0
+
+  static defaultProps = {
+    images: [`http://pets-images.dev-apis.com/pets/none.jpg`],
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            <img
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
