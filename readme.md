2353591bf1a642f74125dd78ae86a1f39974011f
samroodAli <devsamrood@gmail.com>
2021-03-30 07:20:25 +0530

    release 1.1 fix:parcel build script public url

--- readme.md
+++
@@ -1,4 +0,0 @@
-Documenting what I did:
-npm init
-npm install -D prettier
-npm install -D eslint@7.18.0 eslint-config-prettier@8.1.0
--- src/ThemeContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const ThemeContext = createContext(["green", function typeOfSetContext() {}]); //this is for typescript:what its type is going to be,from useState, a string and a function
-
-export default ThemeContext;
